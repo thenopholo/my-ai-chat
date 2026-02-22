b973b82baab5a58b88ad1dde22958b6af3f9e5fa
Rodrigo Thenopholo <thenopholo92@gmail.com>
2026-02-22 00:18:56 -0300

    feat: update font to Nunito Sans and implement theme provider in App component

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,8 +1,16 @@
-"use client";
-
 import "@crayonai/react-ui/styles/index.css";
-import { C1Chat } from "@thesysai/genui-sdk";
+import "./index.css";
+import { C1Chat, ThemeProvider } from "@thesysai/genui-sdk";
+import { themePresets } from "@crayonai/react-ui";
 
 export default function App() {
-  return <C1Chat apiUrl="/api/chat" />;
+  return (
+    <ThemeProvider
+      theme={themePresets.charcoal.theme}
+      darkTheme={themePresets.charcoal.darkTheme}
+      mode="dark"
+    >
+      <C1Chat apiUrl="/api/chat" />
+    </ThemeProvider>
+  );
 }
